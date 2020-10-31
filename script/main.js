4e1a4ed1532ed615fb2cb4c290d192bf1a6b7bf9
Richard Williams <richelliswilliams@gmail.com>
2020-10-30 19:13:03 -0700

    add restart button and logic to player turn switch

--- script/main.js
+++ script/main.js
@@ -19,8 +19,63 @@
 
 /*----- app's state (variables) -----*/
 
+let currentBoard = [null,null,null,null,null,null,null,null];
+let gameStatus;
+let player1Board = [];
+let player2Board = [];
+
+
 /*----- cached element references -----*/
+
+const cellEl = document.querySelectorAll('.grid-container > *');
+
+console.log(cellEl[1].id) // ----> Gets specific ID for cell.
+
+
 
 /*----- event listeners -----*/
 
+document.querySelector('.grid-container').addEventListener('click', handleCellClick);
+
+document.getElementById('reset').addEventListener('click',init);
+
 /*----- functions -----*/
+
+init();
+
+function handleCellClick(evt) {
+    console.log(evt.target)
+    cellId = evt.target.id;
+
+    //if (player1's turn) -->target colors['1']
+    if(player1Board.length === player2Board.length) {
+        player1Board.push(cellId);
+        console.log(player1Board);
+         } else {
+            player2Board.push(cellId);
+            console.log(player2Board);
+         }
+      
+
+
+    //if (player2's turn) -->target colors['-1'] //setWhatever
+
+    //checkWincondition. search in player1's grid for winningCombo
+    render();
+};
+
+function render() {
+
+}
+
+function clearBoard() {
+    
+}
+
+
+function init() {
+    playerTurn = 1;
+
+    clearBoard();
+    render();
+}
