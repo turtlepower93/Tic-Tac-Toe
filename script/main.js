1ef4f549170a88e00dcb0a40fc63da05e08ecd88
Richard Williams <richelliswilliams@gmail.com>
2020-10-30 18:19:11 -0700

    add constants colors and combo

--- script/main.js
+++ script/main.js
@@ -1,6 +1,21 @@
 /*----- constants -----*/
 
+    const colors = {
+        'null': null,
+        '1': 'blue',
+        '-1': 'red'
+    }
 
+    const winningcombos = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
 
 /*----- app's state (variables) -----*/
 
