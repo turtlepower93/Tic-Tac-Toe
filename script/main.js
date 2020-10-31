edf183d646874da260dc6e3a22b048dcfe290aa6
Richard Williams <richelliswilliams@gmail.com>
2020-10-30 18:10:27 -0700

    draw grids

---
+++ script/main.js
@@ -0,0 +1,11 @@
+/*----- constants -----*/
+
+
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
